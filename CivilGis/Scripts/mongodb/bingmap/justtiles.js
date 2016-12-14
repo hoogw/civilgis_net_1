761d9b98d3e39e7208cbcb8b21c67a2bf584b7b2
hoogw <hoogw@hotmail.com>
2016-12-14 15:58:26 -0800

    postgis

---
+++ CivilGis/Scripts/mongodb/bingmap/justtiles.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+function just_add_tiles() {
+
+    init_tiling();
+
+    document.getElementById("ajaxload").style.display = "none";
+    document.getElementById("title_info").style.display = "none";
+   
+   
+}
+
+
+
+
+function initialize() {
+    
+    initial_location = set_initial_location($("#areaID").val());
+
+
+    init_base_map();
+
+    add_area_boundary($("#areaID").val());
+
+
+     geocoding();
+      
+     just_add_tiles();
+
+    }// initialize
+    
+    
+    
+    
+    
+    
+    
